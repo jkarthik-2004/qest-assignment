196fa102247283c25823f15304da163407629274
karthik <jkarthik1947@gmail.com>
2024-12-20 13:20:12 +0530

    Added routes to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import PricingPage from './pages/PricingPage'
+import SupportPage from './pages/SupportPage'
 
 export default function App() {
   return (
-    <div className='text-red-600'>App</div>
+    <Routes>
+      <Route path='/' element={<LandingPage/>}/>
+      <Route path='/pricing' element={<PricingPage/>}/>
+      <Route path='/support' element={<SupportPage/>}/>
+    </Routes>
   )
 }
